638c8d181af0e445ec2fd924ba4735b0ff3e3d41
srdjanRakic <srg_ian@hotmail.com>
2017-09-23 19:08:38 +0200

    New components added.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,11 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
+import Home from '@/components/Home';
+import Meetups from '@/components/Meetup/Meetups';
+import CreateMeetup from '@/components/Meetup/CreateMeetup';
+import Profile from '@/components/User/Profile';
+import SignUp from '@/components/User/SignUp';
+import SignIn from '@/components/User/SignIn';
 
 Vue.use(Router);
 
@@ -8,8 +13,33 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/meetups',
+      name: 'Meetups',
+      component: Meetups,
+    },
+    {
+      path: '/meetups/new',
+      name: 'CreateMeetup',
+      component: CreateMeetup,
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+    },
+    {
+      path: '/signup',
+      name: 'SignUp',
+      component: SignUp,
+    },
+    {
+      path: '/signin',
+      name: 'SignIn',
+      component: SignIn,
     },
   ],
 });

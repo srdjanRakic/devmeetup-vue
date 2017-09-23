187fbf0c8d9a3385ecdf5d4b2c49c04be7b5db03
srdjanRakic <srg_ian@hotmail.com>
2017-09-23 19:08:06 +0200

    IMenuItmes added.

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <v-app>
     <v-navigation-drawer v-model="sideNav" temporary>
       <v-list>
-        <v-list-tile>    
+        <v-list-tile v-for="item in menuItems" :key="item.title">    
           <v-list-tile-action>
-            <v-icon>supervisor_account</v-icon>
+            <v-icon>{{item.icon}}</v-icon>
           </v-list-tile-action>
-          <v-list-tile-content>View Meetups</v-list-tile-content>
+          <v-list-tile-content>{{item.title}}</v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
@@ -18,14 +18,14 @@
       <v-toolbar-title>DevMeetup</v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat>
-          <v-icon left>supervisor_account</v-icon>
-          View Meetups
+        <v-btn flat v-for="item in menuItems" :key="item.title">
+          <v-icon left>{{item.icon}}t</v-icon>
+            {{item.title}}
         </v-btn>
       </v-toolbar-items>
     </v-toolbar>
     <main>
-
+      <router-view></router-view>
     </main>
   </v-app>
 </template>
@@ -35,6 +35,13 @@
     data() {
       return {
         sideNav: false,
+        menuItems: [
+          { icon: 'supervisor_account', title: 'View Meetups' },
+          { icon: 'room', title: 'Organize Meetup' },
+          { icon: 'person', title: 'Profile' },
+          { icon: 'face', title: 'Sign up' },
+          { icon: 'lock_open', title: 'Sign in' },
+        ],
       };
     },
   };

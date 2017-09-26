6d708f306cb3411bc91fefcbdf64c212c80db934
Srdjan Rakic <srg_ian@hotmail.com>
2017-09-26 10:17:17 +0200

    Firebase and Signup component added.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import * as firebase from 'firebase';
 import Vue from 'vue';
 import Vuetify from 'vuetify';
 import './stylus/main.styl';
@@ -5,6 +6,7 @@ import App from './App';
 import router from './router';
 import { store } from './store';
 import DateFilter from './filters/date';
+
 
 Vue.use(Vuetify);
 Vue.config.productionTip = false;
@@ -17,4 +19,13 @@ new Vue({
   router,
   store,
   render: h => h(App),
+  created() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDbImZ1jGCUsVxeTg3WltMvl5BXSJ95l6U',
+      authDomain: 'devmeetup-55085.firebaseapp.com',
+      databaseURL: 'https://devmeetup-55085.firebaseio.com',
+      projectId: 'devmeetup-55085',
+      storageBucket: 'devmeetup-55085.appspot.com',
+    });
+  },
 });
